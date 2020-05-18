3ddaa113f3e66219a72e17809ac8b9150f890881
mcn <2572482562@qq.com>
2020-05-18 20:29:40 +0800

    提交

---
+++ sms/src/main/java/com/offcn/SendSms.java
@@ -0,0 +1,43 @@
+package com.offcn;
+
+import com.aliyuncs.CommonRequest;
+import com.aliyuncs.CommonResponse;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.exceptions.ServerException;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
+/*
+pom.xml
+<dependency>
+  <groupId>com.aliyun</groupId>
+  <artifactId>aliyun-java-sdk-core</artifactId>
+  <version>4.0.3</version>
+</dependency>
+*/
+public class SendSms {
+    public static void main(String[] args) {
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI4FfefAoZCEDgm2fbJzRr", "jB8tNIqMDAM15WRZhtCVfFv3firouo");
+        IAcsClient client = new DefaultAcsClient(profile);
+
+        CommonRequest request = new CommonRequest();
+        request.setSysMethod(MethodType.POST);
+        request.setSysDomain("dysmsapi.aliyuncs.com");
+        request.setSysVersion("2017-05-25");
+        request.setSysAction("SendSms");
+        request.putQueryParameter("RegionId", "cn-hangzhou");
+        request.putQueryParameter("PhoneNumbers", "15839034353");
+        request.putQueryParameter("SignName", "优乐选电商平台");
+        request.putQueryParameter("TemplateCode", "SMS_184626465");
+        request.putQueryParameter("TemplateParam", "{\"code\":\"88889\"}");
+        try {
+            CommonResponse response = client.getCommonResponse(request);
+            System.out.println(response.getData());
+        } catch (ServerException e) {
+            e.printStackTrace();
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+    }
+}
